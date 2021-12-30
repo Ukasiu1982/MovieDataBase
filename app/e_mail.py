c33560cf2b0c54af729b88042a5576e6f97a3608
Łukasz Ł <ukaszek1001@gmail.com>
2021-12-30 16:13:14 +0100

    dodano folder app

---
+++ app/e_mail.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+
+EMAIL_FROM = os.environ.get('EMAIL_FROM')
+EMAIL_FROM_PASS = os.environ.get('EMAIL_FROM_PASS')
+EMAIL_TO = os.environ.get('EMAIL_TO')
+
+
+#  dla konta google trzeba wylaczyc zabezpiecznie dostepu dla mniej bezpiecznej aplikacji
+def send_email(provider, title):
+    email_user = EMAIL_FROM
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email_user, EMAIL_FROM_PASS)
+
+    message = f"{title} jest dostepne w serwisie: {provider['name']}"
+    server.sendmail(EMAIL_FROM, EMAIL_TO, message)
+    print('email to: ', EMAIL_TO)
+    print('message: ', message)
+    server.quit()
